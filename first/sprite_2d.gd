90c9e3c0d8a9c5bfaaa5744c7429a660fce05524
Amer Khalid <amer@Amers-Mac-mini.local>
2023-08-19 09:57:14 -0500

    finished step by step tutorial

--- first/sprite_2d.gd
+++ first/sprite_2d.gd
@@ -8,17 +8,19 @@ var angular_speed = PI
 func _init():
 	print("Hello, world!")
 	
-func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-
-	rotation += angular_speed * direction * delta
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
 	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-		
+	
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
+
+func _on_timer_timeout():
+	visible = not visible	
